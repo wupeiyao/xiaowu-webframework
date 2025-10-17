da98842c9bfa4b4b00c742119f136d0f1027748d
wupy <2034037932@qq.com>
2025-10-17 11:07:23 +0800

    优化

--- src/main/java/org/xiaowu/wpywebframework/object/bucket/Bucket.java
+++ src/main/java/org/xiaowu/wpywebframework/object/bucket/Bucket.java
@@ -14,20 +14,4 @@ public interface Bucket {
         return null;
     }
 
-    /**
-     * 是否是临时的桶
-     *
-     * @return true临时 false 永久
-     */
-    default boolean temporary() {
-        return false;
-    }
-
-    /**
-     * @return 临时文件过期时间, 默认3天
-     */
-    default int overdue() {
-        return 0;
-    }
-
 }
